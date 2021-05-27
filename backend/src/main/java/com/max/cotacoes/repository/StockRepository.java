918bd1dd5e2f3f69c5855ac6c06319ee2f7936b4
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-05-27 16:05:50 -0300

    Model and Repository created

---
+++ backend/src/main/java/com/max/cotacoes/repository/StockRepository.java
@@ -0,0 +1,9 @@
+package com.max.cotacoes.repository;
+
+import com.max.cotacoes.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
+}
